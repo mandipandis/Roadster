1f3539b7a96d7cbdeab1618c4f4765258724d130
Amanda <amandaholtmo@hotmail.com>
2025-02-09 13:53:44 +0100

    yes

--- script_part1b.py
+++ script_part1b.py
@@ -1,2 +1,15 @@
+import matplotlib.pyplot as plt
 import roadster
-help ( roadster . load_route )
+#help (roadster . load_route )
+
+
+
+distance_km, speed_kmph = roadster.load_route('speed_anna.npz')
+
+v = roadster.velocity(0,'speed_anna')
+
+plt.plot(distance_km, speed_kmph, "ro")
+distance_km, speed_kmph = roadster.load_route('speed_elsa.npz')
+plt.plot(distance_km,speed_kmph, "bo")
+
+plt.show()
