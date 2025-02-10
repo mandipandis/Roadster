b2f414c5d5c72af2b98dafcbf4b5192d3baf477b
Amanda <amandaholtmo@hotmail.com>
2025-02-10 12:20:18 +0100

    nytt

--- script_part1b.py
+++ script_part1b.py
@@ -1,8 +1,19 @@
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
 import roadster
 #help (roadster . load_route )
 
+distance_km, speed_kmph = roadster.load_route('speed_anna.npz')
+
+v = roadster.velocity(0,'speed_anna')
+
+plt.plot(distance_km, speed_kmph, "ro")
+distance_km, speed_kmph = roadster.load_route('speed_elsa.npz')
+plt.plot(distance_km,speed_kmph, "bo")
+
+plt.show()
+import matplotlib.pyplot as plt
+import roadster
+#help (roadster . load_route )
 
 
 distance_km, speed_kmph = roadster.load_route('speed_anna.npz')
@@ -14,19 +25,3 @@ distance_km, speed_kmph = roadster.load_route('speed_elsa.npz')
 plt.plot(distance_km,speed_kmph, "bo")
 
 plt.show()
-=======
-import matplotlib.pyplot as plt
-import roadster
-#help (roadster . load_route )
-
-
-distance_km, speed_kmph = roadster.load_route('speed_anna.npz')
-
-v = roadster.velocity(0,'speed_anna')
-
-plt.plot(distance_km, speed_kmph, "ro")
-distance_km, speed_kmph = roadster.load_route('speed_elsa.npz')
-plt.plot(distance_km,speed_kmph, "bo")
-
-plt.show()
->>>>>>> 6f37f59dfee53125b2790495168c201354c9b800
