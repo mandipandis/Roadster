6540d701f286abcdfdfe824a593882d5df605304
Amanda <amandaholtmo@hotmail.com>
2025-02-10 12:51:16 +0100

    1b klar

--- script_part1b.py
+++ script_part1b.py
@@ -1,27 +1,18 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import roadster
-#help (roadster . load_route )
+from roadster import velocity 
 
+#distance_km, speed_kmph = roadster.load_route('speed_elsa.npz')
+#plt.plot(distance_km, speed_kmph, "ro")
 distance_km, speed_kmph = roadster.load_route('speed_anna.npz')
-
-v = roadster.velocity(0,'speed_anna')
-
-plt.plot(distance_km, speed_kmph, "ro")
-distance_km, speed_kmph = roadster.load_route('speed_elsa.npz')
 plt.plot(distance_km,speed_kmph, "bo")
 
-plt.show()
-import matplotlib.pyplot as plt
-import roadster
-#help (roadster . load_route )
+x_fine = np.linspace(min(distance_km), max(distance_km), 1000)
+v_interpolated = [velocity(x, 'speed_anna') for x in x_fine] #från gpt- den gör en lista typ men måste försöka fatta
+plt.plot(x_fine, v_interpolated, label='Interpolerad funktion', color='green')
 
-
-distance_km, speed_kmph = roadster.load_route('speed_anna.npz')
-
-v = roadster.velocity(0,'speed_anna')
-
-plt.plot(distance_km, speed_kmph, "ro")
-distance_km, speed_kmph = roadster.load_route('speed_elsa.npz')
-plt.plot(distance_km,speed_kmph, "bo")
-
+plt.xlabel('Sträcka [km]')
+plt.ylabel('Hastighet [km/h]')
+plt.legend()
 plt.show()
